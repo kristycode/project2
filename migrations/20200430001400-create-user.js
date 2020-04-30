3886527eda28e56af9e77d194b581dec88abea2f
Kristy Farmer <kristyfarmerschool@gmail.com>
2020-04-29 19:25:49 -0500

    Created database and file structure

---
+++ migrations/20200430001400-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      state: {
+        type: Sequelize.BOOLEAN
+      },
+      birth: {
+        type: Sequelize.DATE
+      },
+      card: {
+        type: Sequelize.INTEGER
+      },
+      role: {
+        type: Sequelize.ENUM('Admin', 'Guest')
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
