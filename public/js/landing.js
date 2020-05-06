9e61e0d43cc2c760bdfabaeee9e4e9905adba512
Lemonade Kidd <smejia03@gmail.com>
2020-05-06 11:27:32 -0500

    troubleshooting pixabay api

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,6 +1,7 @@
 // on click, on click function with keyword this to find data-id then throw that attr into an ajax call
 $(document).ready(function () {
-  $(".commentBtn").click(function () {
+  $(".commentBtn").on("submit", function (e) {
+    e.preventDefault();
     console.log("comments was clicked");
   });
 });
--- public/js/newPost.js
+++ public/js/newPost.js
@@ -1,22 +1,18 @@
 $(document).ready(function () {
-  // require("dotenv").config();
-
   var bodyInput = $("#body");
   var titleInput = $("#title");
   var dreamForm = $("#dreamPost");
   var postCategorySelect = $("#category");
-  // var postColorSelect = $("#color");
   var qCategory = postCategorySelect.val();
-  // var qColor = postColorSelect.val();
   var apiKey = "16264583-da5b3c8db2ac66bc7ed9ce8a3&q";
   var pixaBay = `https://pixabay.com/api/?key=${apiKey}&q=${qCategory}&safesearch=true&image_type=photo`;
-  // var pixaBay = `https://pixabay.com/api/?key=${apiKey}&q=${qCategory}+${qColor}&safesearch=true&image_type=photo`;
 
   console.log(pixaBay);
+
   $(dreamForm).on("submit", function handleFormSubmit (event) {
     console.log(pixaBay);
     event.preventDefault();
-    if (!titleInput.val().trim() || !bodyInput.val().trim()) {
+    if (!titleInput.val().trim() || !bodyInput.val().trim() || !postCategorySelect.val().trim()) {
       return;
     }
     userIdFunc();
@@ -26,7 +22,7 @@ $(document).ready(function () {
     try {
       const userData = await $.get("/api/user_data");
       const photoURL = await $.get(pixaBay);
-      var res = await photoURL.hits[0].webformatURL;
+      var res = await photoURL.hits[1].webformatURL;
       // if (userData) {
       const userId = userData.id;
       console.log(res);
@@ -44,7 +40,6 @@ $(document).ready(function () {
       console.error(error);
     }
   };
-
   function submitPost (post) {
     $.post("/api/posts/", post, function () {
       window.location.href = "/dreamstream-home";
