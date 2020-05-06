d3a5b2e4edd77328f6fb623b411e1454e9939c1c
Kristy Farmer <kristyfarmerschool@gmail.com>
2020-05-05 23:37:10 -0500

    I've got the pixabay API to take in search query input but for some reason it doesn't render what was entered, and then posts the previous query to the next post the user makes. Cannot figure out why

--- public/js/newPost.js
+++ public/js/newPost.js
@@ -3,19 +3,20 @@ $(document).ready(function () {
   var titleInput = $("#title");
   var dreamForm = $("#dreamPost");
   var postCategorySelect = $("#category");
-  var postColorSelect = $("#color");
+  // var postColorSelect = $("#color");
   var qCategory = postCategorySelect.val();
-  var qColor = postColorSelect.val();
+  // var qColor = postColorSelect.val();
   var apiKey = "16264583-da5b3c8db2ac66bc7ed9ce8a3&q";
-  var pixaBay = `https://pixabay.com/api/?key=${apiKey}&q=${qCategory}+${qColor}&safesearch=true&image_type=photo`;
+  var pixaBay = `https://pixabay.com/api/?key=${apiKey}&q=${qCategory}&safesearch=true&image_type=photo`;
+  // var pixaBay = `https://pixabay.com/api/?key=${apiKey}&q=${qCategory}+${qColor}&safesearch=true&image_type=photo`;
 
+  console.log(pixaBay);
   $(dreamForm).on("submit", function handleFormSubmit (event) {
     console.log(pixaBay);
     event.preventDefault();
     if (!titleInput.val().trim() || !bodyInput.val().trim()) {
       return;
     }
-
     userIdFunc();
   });
 
@@ -23,7 +24,7 @@ $(document).ready(function () {
     try {
       const userData = await $.get("/api/user_data");
       const photoURL = await $.get(pixaBay);
-      const res = await photoURL.hits[0].webformatURL;
+      var res = await photoURL.hits[0].webformatURL;
       // if (userData) {
       const userId = userData.id;
       console.log(res);
@@ -31,10 +32,11 @@ $(document).ready(function () {
         post_title: titleInput.val().trim(),
         post_content: bodyInput.val().trim(),
         post_category: postCategorySelect.val(),
-        post_color: postColorSelect.val(),
+        // post_color: postColorSelect.val(),
         url_image: res,
         UserId: userId
       };
+      console.log(newPost);
       await submitPost(newPost);
     } catch (error) {
       console.error(error);
