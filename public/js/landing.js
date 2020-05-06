277fcb6bb38afcaf13f992c7665fc6813b095a3c
Lemonade Kidd <smejia03@gmail.com>
2020-05-05 22:46:17 -0500

    comments are work in progess

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,16 +1,6 @@
 // on click, on click function with keyword this to find data-id then throw that attr into an ajax call
 $(document).ready(function () {
-    $("this.commentBtn").on("click", (e) => {
-        e.preventDefault();
-      console.log("comments button was clicked");     
-    //   $.ajax({
-    //     type: "PUT",
-    //     url: "/api/user-dream/" + id
-    //   }).then(
-    //     function(data) {
-    //       console.log(data);
-    //       location.reload();
-    //     }
-    //   )
-    // })
-};
+  $(".commentBtn").click(function () {
+    console.log("comments was clicked");
+  });
+});
